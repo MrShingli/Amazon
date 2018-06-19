be4d9e36bc71c932f57d35e1b5bd0767bde60a39
Marko Vasilic <vasilicsingli@gmail.com>
2018-06-19 21:35:00 +0200

    Add files via upload

---
+++ Amazon/ChromeOpen.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon
+{
+    class ChromeOpen
+    {
+        public static IWebDriver browser = new ChromeDriver();
+        public Data data;
+        public ChromeOpen()
+        {
+
+            this.data = new Data();
+        }
+    }
+}
