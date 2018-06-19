fd3d3c9d2996f2d2c6d9e97163a69e3e29aed747
Marko Vasilic <vasilicsingli@gmail.com>
2018-06-19 21:22:14 +0200

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazon
+{
+    class Data
+    {
+        public string Email = "marko.automatsko@gmail.com";
+        public string Password = "automatsko1";
+    }
+}
